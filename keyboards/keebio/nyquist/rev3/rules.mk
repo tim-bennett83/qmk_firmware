664e399ca8bf220ca04f0faaf98acb9c7015d076
Timothy Bennett <timothy.bennett@rallyhealth.com>
2022-10-25 21:12:13 -0700

    disable rgb for space

--- keyboards/keebio/nyquist/rev3/rules.mk
+++ keyboards/keebio/nyquist/rev3/rules.mk
@@ -13,9 +13,9 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 
 SPLIT_KEYBOARD = yes
 LAYOUTS = ortho_5x12 ortho_4x12
